e33ab1cecb03bf407c2d2079d3def5abba9b4469
Homeshu Harshit Bennett <89681873+Homeshubennet007@users.noreply.github.com>
2024-02-27 19:24:12 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+//primitive data type= call by value
+// 7 types : String,Number,Boolean,null,undefined,Symbol.BigInt
+
+const score = 100
+const scoreValue =100.3
+const isLoggedIn =false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(userEmail);
+
+const bigNumber = 34567823456745678n
+
+//Reference(Non-primitive data type) = reference can be directly allocated in the memory
+    //-Array,objects,Functions
+
+
+ //is javascript dynamically typed or statically typed language  
+    
