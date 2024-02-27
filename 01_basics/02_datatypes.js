36fa94a1cc6250a09de476dfa6c7a47e0dd34d7b
Homeshu Harshit Bennett <89681873+Homeshubennet007@users.noreply.github.com>
2024-02-27 09:32:35 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";   //treat all js code as newer version
+
+//alert(3+3) //we are using node js not browser
+
+
+console.log("honey")
+
+let name = "honey"
+let number = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53
+// bigint 
+// string => ""
+// boolean  => true/false
+// null- stand alone value as well as a datatype// it is a representation of empty value
+// undefined - 
+// symbol => unique
+
+
+
+//object 
+
+
+
+console.log(typeof "honey");
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
