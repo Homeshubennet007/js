0b354a367116237199bcb09c0d95086690713e6e
Homeshu Harshit Bennett <89681873+Homeshubennet007@users.noreply.github.com>
2024-02-28 12:28:47 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,40 @@
+let myName = "homeshu"
+let myAge = 26
+
+//console.log(myName+myAge);  <-- old school
+
+console.log(`Hello my name is ${myName} and my age is ${myAge}`);
+
+
+//string declaration
+const myGame = new String('gtafiveisthegame')
+//console.log(myGame[0]);
+//console.log(myGame.__proto__);
+
+
+//console.log(myGame.length)
+//console.log(myGame.toUpperCase());
+//console.log(myGame.charAt(3));
+//console.log(myGame.indexOf('i'));
+
+
+//const newString = myGame.substring(0,3)
+//console.log(newString);
+
+//const anotherString = myGame.slice(-6,3)
+//console.log(anotherString); 
+//slice accepts negative value
+
+
+//const newStringOne = "     homeshu      "
+//console.log(newStringOne);
+//console.log(newStringOne.trim());
+
+const url = "https://bababennett.com/baba%20bennett"
+
+//console.log(url.replace('%20','-'));
+
+//console.log(url.includes('baba'));
+
+
+console.log(myGame.split('-,/'));
